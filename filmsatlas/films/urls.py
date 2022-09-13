bfe1924a54d83c1973533e79411ec0f348385254
TolyaLQs <sdtolya@gmail.com>
2022-09-13 16:14:47 +0500

    views index 1.5

---
+++ filmsatlas/films/urls.py
@@ -0,0 +1,16 @@
+from django.urls import re_path
+import filmsatlas.films.views as films
+
+app_name = 'films'
+
+urlpatterns = [
+    re_path('$', films.index, name='index'),
+    re_path('genres/$', films.genres, name='genres'),
+    re_path('genre/(?P<genre>.*\s*)/$', films.genre, name='genre'),
+    re_path('sort-year/(?P<year>.*\s*)/$)', films.year, name='year'),
+    re_path('sort-country/(?P<country>.*\s*)/$', films.country, name='country'),
+    re_path('collections/$', films.collections, name='collections'),
+    re_path('collection/(?P<collection>.*\s*)/$', films.collection, name='collection'),
+    re_path('film/(?P<film>.*\s*)/$', films.film, name='film'),
+    re_path('search/(?P<search>.*\s*)/$', films.search, name='search'),
+]
